59e99bc6843962d3d594b70fad5a5100561de318
guidoglorioso01 <guido.glorioso@frba.utn.edu.ar>
2023-07-06 11:40:49 -0300

    Agrego funcionalidad de medir RC por metodo de FFT

--- InstVirtualLib/mediciones.py
+++ InstVirtualLib/mediciones.py
@@ -15,6 +15,7 @@ Todos los calculos de los
 from math import floor
 from matplotlib import pyplot as plt
 import numpy as np
+import scipy.signal as dsp
 
 
 class Mediciones():
@@ -63,3 +64,79 @@ class Mediciones():
         thd = np.sqrt(np.sum(harmonics_values[1:]**2))/harmonics_values[0]
  
         return thd
+    
+    def calculo_Capacitor(self, valor_r, tiempo, tension_r, tension_gen, modo = "TIEMPO"):
+        '''
+        Calculo de capacitor por distintos metodos
+        
+        Parameters
+        ----------
+        valor_r : INT
+            valor de resistencia en ohms.
+        tiempo : vector
+            Vector de tiempos de muestras.
+        tension_r : Vector
+            Vector de valores de tension [V] en la resistencia.
+        tension_gen : Vector
+            Vector de valores de tension [V] en el generador
+        modo : {TIEMPO; LISSAJ; POT; FFT} string en mayusculas
+           Se elije de que modo se calculara el capacitor (Default = TIEMPO)
+
+        Returns
+        -------
+        valor del capacitor calculado por el metodo indicado
+
+        '''
+        # Determino que metodo de medicion utilizo
+        if modo == "FFT": 
+            
+            # obtengo la frecuencia de muestreo
+            fs=1/(tiempo[1]-tiempo[0]) 
+            
+            # Rango del espectro de fourier
+            fcia=np.linspace(0,fs/2,len(tiempo)//2)
+            
+            #Aplico una ventana tipo flat top para mayor presicion
+            window = dsp.flattop(len(fcia)) # Ventana flat top
+            tension_r *= window
+            tension_gen *= window
+            
+            # obtengo modulo de transformada de fourier de las señales en cuestion
+            fft_r   = np.abs(np.fft.fft(tension_r)) 
+            fft_r   = fft_r[0:len(fft_r)//2] # elimino espectro repetido
+            fft_r   /= len(fcia) # desnormalizo la fft
+            
+            fft_gen = np.abs(np.fft.fft(tension_gen))
+            fft_gen = fft_gen[0:len(fft_gen)//2] # elimino espectro repetido
+            fft_gen /= len(fcia) # desnormalizo la fft
+            
+            #obtengo valores maximos de amplitud y sus fases
+            valor_max_fft_r     = np.max(fft_r) # valor pico de la señal
+            posicion_max_fft_r  = np.where(fft_r == np.max(fft_r))
+            angulo_fft_r =np.angle(np.fft.fft(tension_r)[posicion_max_fft_r])
+            
+            valor_max_fft_gen     = np.max(fft_gen) # valor pico de la señal
+            posicion_max_fft_gen  = np.where(fft_gen == np.max(fft_gen))
+            angulo_fft_gen =np.angle(np.fft.fft(tension_gen)[posicion_max_fft_gen])
+            
+            #calculo del capacitor
+            
+            frecuencia_Señal = fcia[posicion_max_fft_r] # Frecuencia del pico (de la senoidal)
+            itot= valor_max_fft_r/valor_r
+            zt = (valor_max_fft_gen/itot) #No se multiplica las ventanas por el coef de correccion ya que se cancelan
+
+            angle_total = angulo_fft_r - angulo_fft_gen 
+            xc = zt * np.sin(angle_total) 
+            valor_cap = 1/(2 * np.pi * frecuencia_Señal *xc) #valor calculado
+
+        elif modo == "POT":     # Falta implementar
+            valor_cap = 0
+            pass
+        elif modo == "LISSAJ":  # Falta implementar
+            valor_cap = 0
+            pass
+        elif modo == "TIEMPO":  # Falta implementar
+            valor_cap = 0
+            pass
+
+        return valor_cap
